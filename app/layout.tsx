8418d754a4c10b69e477c988ae4e32f1e0747c62
Max <maksim.bogomyakov@gmail.com>
2023-12-10 03:14:28 +0300

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,5 @@
+import { Providers } from '@/redux/provider'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +12,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="ru">
+      <body>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
     </html>
   )
 }
