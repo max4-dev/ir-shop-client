51d970c9faa4dba2703f5d886891fdd512075912
Max <maksim.bogomyakov@gmail.com>
2024-02-13 07:37:01 +0300

    added adaptive main page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,18 +1,35 @@
 'use client'
 
+import Image from 'next/image';
+import { useState } from 'react';
+
 import { Filter, Sort } from '@/components/widgets';
 import { ProductList } from '@/components/layouts';
+import { Popup } from '@/components/shared/ui';
+import { useWindowSize } from '@/hooks/useWindowSize';
 
 import styles from './Home.module.scss';
 
 export default function Home() {
+  const { width } = useWindowSize();
+  const [isFilterOpen, setFilterOpen] = useState<boolean>(false);
+  const isMobile = Boolean(width && width <= 1280);
 
   return (
     <div className={styles.home}>
       <div className="container">
-        <Sort />
+        <div className={styles.top}>
+          {isMobile && <button className={styles.filterButton} onClick={() => setFilterOpen(prevState => !prevState)}>
+            <Image src="images/icons/filter.svg" width={30} height={30} alt="Иконка фильтрации" />
+          </button>}
+          <Sort className={styles.sort} />
+        </div>
         <div className={styles.content}>
-          <Filter />
+          {isMobile ? 
+            <Popup isOpen={isFilterOpen} setIsOpen={setFilterOpen}>
+              <Filter className={styles.filter} />
+            </Popup>
+          : <Filter />}
           <ProductList />
         </div>
       </div>
